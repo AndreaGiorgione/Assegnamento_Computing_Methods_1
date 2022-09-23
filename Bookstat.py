6b165239b9f318ad0338b882ed8a33382aea96d1
AndreaGiorgione <anadndreagiorgione98@gmail.com>
2022-09-23 08:46:50 +0200

    Python program initialization

---
+++ Bookstat.py
@@ -0,0 +1,22 @@
+"""First assignment for the CMEPDA course, 2022/23.
+"""
+
+import argparse
+
+
+def process(file_path):
+    """
+    """
+    print(f'Opening input file {file_path}...')
+    with open(file_path, 'r') as input_file:
+        text = input_file.read()
+    print(text)
+    print('Done.')
+
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Print some book statistics')
+    parser.add_argument('infile', type=str, help='path to the input file')
+    args = parser.parse_args(args)
+    process(args.infile)
