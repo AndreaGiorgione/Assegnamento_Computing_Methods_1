2f9850d457dc8a9e226e317a0d4ce4a7c87a6a2f
AndreaGiorgione <anadndreagiorgione98@gmail.com>
2022-09-26 11:59:43 +0200

    Restore of Python file to initial version

--- Bookstat.py
+++ Bookstat.py
@@ -3,7 +3,6 @@
 
 import argparse
 
-
 def process(file_path):
     """
     """
@@ -18,5 +17,5 @@ def process(file_path):
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Print some book statistics')
     parser.add_argument('infile', type=str, help='path to the input file')
-    args = parser.parse_args(args)
+    args = parser.parse_args()
     process(args.infile)
